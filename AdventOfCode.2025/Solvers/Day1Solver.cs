7d6d37a24229cdc7adec9ba08d6ef5ff57c74935
Jamie Brown <jamie.brown@ion.fm>
2025-12-01 14:16:26 +0000

    add instruction model

--- AdventOfCode.2025/Solvers/Day1Solver.cs
+++ AdventOfCode.2025/Solvers/Day1Solver.cs
@@ -20,4 +20,19 @@ public class Day1Solver : IAoCSolver
     {
         return "D1P2";
     }
+
+    private record struct Instruction(int Direction, int Value)
+    {
+        public static Instruction Parse(string line)
+        {
+            int direction = line[0] switch
+            {
+                'L' => -1,
+                'R' => 1,
+                _ => throw new ArgumentOutOfRangeException(nameof(line), line, "Line should start with 'L' or 'R'"),
+            };
+            int value = int.Parse(line[1..]);
+            return new Instruction(direction, value);
+        }
+    }
 }
