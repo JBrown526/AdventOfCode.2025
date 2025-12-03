062dd4c58f30fd3032672d726fdae00b7400e5c0
Jamie Brown <jamie.brown@ion.fm>
2025-12-03 10:40:00 +0000

    part 1

--- AdventOfCode.2025/Solvers/Day3Solver.cs
+++ AdventOfCode.2025/Solvers/Day3Solver.cs
@@ -16,7 +16,39 @@ public class Day3Solver : IAoCSolver
 
     public string Part1()
     {
-        return "D3P1";
+        IEnumerable<string> data = _dataProvider.GetData(Day);
+
+        int totalJoltage = 0;
+        foreach (string line in data)
+        {
+            char highestFirst = '0';
+            char highestSecond = '0';
+            // Check all but the last digit, we'll do a special case check on that in case the last digit is largest
+            foreach (char joltage in line[..^1])
+            {
+                // If the joltage is higher than the first digit, then we have a new high number
+                if (joltage > highestFirst)
+                {
+                    highestFirst = joltage;
+                    highestSecond = '0';
+                    continue;
+                }
+
+                if (joltage > highestSecond)
+                {
+                    highestSecond = joltage;
+                }
+            }
+
+            if (line[^1] > highestSecond)
+            {
+                highestSecond = line[^1];
+            }
+
+            totalJoltage += int.Parse(new string([highestFirst, highestSecond]));
+        }
+
+        return totalJoltage.ToString();
     }
 
     public string Part2()
