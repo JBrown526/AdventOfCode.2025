e49ae279ce0064dddc45e76d1cbc578c3a4dfbcc
Jamie Brown <jamie.brown@ion.fm>
2025-12-03 18:49:46 +0000

    scaffold

---
+++ AdventOfCode.2025.Tests/Day4Tests.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode2025.DataProviders;
+using AdventOfCode2025.Solvers;
+using FluentAssertions;
+using Moq;
+
+namespace AdventOfCode2025.Tests;
+
+public class Day4Tests
+{
+    private readonly Mock<IDataProvider> _dataProviderMock = new();
+
+    [Theory]
+    [InlineData("D4P1", "TODO")]
+    public void Part1(string expected, params string[] inputs)
+    {
+        _dataProviderMock.Setup(dp => dp.GetData(3)).Returns(inputs);
+
+        Day4Solver solver = new(_dataProviderMock.Object);
+
+        solver.Part1().Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("D4P2", "TODO")]
+    public void Part2(string expected, params string[] inputs)
+    {
+        _dataProviderMock.Setup(dp => dp.GetData(3)).Returns(inputs);
+
+        Day4Solver solver = new(_dataProviderMock.Object);
+
+        solver.Part2().Should().Be(expected);
+    }
+}
---
+++ AdventOfCode.2025/Solvers/Day4Solver.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode2025.DataProviders;
+
+namespace AdventOfCode2025.Solvers;
+
+public class Day4Solver : IAoCSolver
+{
+    private IDataProvider _dataProvider;
+
+    public int Day => 4;
+
+    public Day4Solver(IDataProvider dataProvider)
+    {
+        _dataProvider = dataProvider;
+    }
+
+    public string Part1()
+    {
+        return "D4P1";
+    }
+
+    public string Part2()
+    {
+        return "D4P2";
+    }
+}
