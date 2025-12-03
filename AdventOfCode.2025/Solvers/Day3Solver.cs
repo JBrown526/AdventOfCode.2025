8cfb9e69918eba23e83e425cd056d6fe856e8505
Jamie Brown <jamie.brown@ion.fm>
2025-12-03 10:25:52 +0000

    scaffold solver

---
+++ AdventOfCode.2025/Solvers/Day3Solver.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode2025.DataProviders;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdventOfCode2025.Solvers;
+
+public class Day3Solver : IAoCSolver
+{
+    private readonly IDataProvider _dataProvider;
+
+    public int Day => 3;
+
+    public Day3Solver([FromKeyedServices(DataProviderKind.LineDelimited)] IDataProvider dataProvider)
+    {
+        _dataProvider = dataProvider;
+    }
+
+    public string Part1()
+    {
+        return "D3P1";
+    }
+
+    public string Part2()
+    {
+        return "D3P2";
+    }
+}
