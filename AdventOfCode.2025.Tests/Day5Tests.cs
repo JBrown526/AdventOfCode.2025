ce67b7fcfc8b0773a9980e67a9b8880582e2b3ef
Jamie Brown <jamie.brown@ion.fm>
2025-12-05 10:18:04 +0000

    scaffold

---
+++ AdventOfCode.2025.Tests/Day5Tests.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode2025.DataProviders;
+using AdventOfCode2025.Solvers;
+using FluentAssertions;
+using Moq;
+
+namespace AdventOfCode2025.Tests;
+
+public class Day5Tests
+{
+    private readonly Mock<IDataProvider> _dataProviderMock = new();
+
+    [Theory]
+    [InlineData("3", "3-5", "10-14", "16-20", "12-18", "", "1", "5", "8", "11", "17", "32")]
+    public void Part1(string expected, params string[] turns)
+    {
+        _dataProviderMock.Setup(fh => fh.GetData(5)).Returns(turns);
+
+        Day5Solver solver = new(_dataProviderMock.Object);
+
+        solver.Part1().Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("", "3-5", "10-14", "16-20", "12-18", "", "1", "5", "8", "11", "17", "32")]
+    public void Part2(string expected, params string[] turns)
+    {
+        _dataProviderMock.Setup(fh => fh.GetData(5)).Returns(turns);
+
+        Day5Solver solver = new(_dataProviderMock.Object);
+
+        solver.Part2().Should().Be(expected);
+    }
+}
---
+++ AdventOfCode.2025/Solvers/Day5Solver.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode2025.DataProviders;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdventOfCode2025.Solvers;
+
+public class Day5Solver : IAoCSolver
+{
+    private readonly IDataProvider _dataProvider;
+
+    public Day5Solver([FromKeyedServices(DataProviderKind.LineDelimited)] IDataProvider dataProvider)
+    {
+        _dataProvider = dataProvider;
+    }
+
+    public int Day => 5;
+
+    public string Part1()
+    {
+        return "D5P1";
+    }
+
+    public string Part2()
+    {
+        return "D5P2";
+    }
+}
