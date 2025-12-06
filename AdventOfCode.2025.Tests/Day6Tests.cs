b2dda3a3134bf9ab563c8f37d7778762e10444c6
Jamie <jamiejbrown521@gmail.com>
2025-12-06 12:44:48 +0000

    scaffold

---
+++ AdventOfCode.2025.Tests/Day6Tests.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode2025.DataProviders;
+using AdventOfCode2025.Solvers;
+using FluentAssertions;
+using Moq;
+
+namespace AdventOfCode2025.Tests;
+
+public class Day6Tests
+{
+    private readonly Mock<IDataProvider> _dataProviderMock = new();
+
+    [Theory]
+    [InlineData("4277556", "123 328  51 64 ", " 45 64  387 23 ", "  6 98  215 314", "*   +   *   +  ")]
+    public void Part1(string expected, params string[] input)
+    {
+        _dataProviderMock.Setup(dp => dp.GetData(6)).Returns(input);
+
+        Day6Solver solver = new(_dataProviderMock.Object);
+
+        solver.Part1().Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("4277556", "123 328  51 64 ", " 45 64  387 23 ", "  6 98  215 314", "*   +   *   +  ")]
+    public void Part2(string expected, params string[] input)
+    {
+        _dataProviderMock.Setup(dp => dp.GetData(6)).Returns(input);
+
+        Day6Solver solver = new(_dataProviderMock.Object);
+
+        solver.Part2().Should().Be(expected);
+    }
+}
---
+++ AdventOfCode.2025/Solvers/Day6Solver.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode2025.DataProviders;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdventOfCode2025.Solvers;
+
+public class Day6Solver : IAoCSolver
+{
+    private readonly IDataProvider _dataProvider;
+
+    public int Day => 6;
+
+    public Day6Solver([FromKeyedServices(DataProviderKind.LineDelimited)] IDataProvider dataProvider)
+    {
+        _dataProvider = dataProvider;
+    }
+
+    public string Part1()
+    {
+        return "D6P1";
+    }
+
+    public string Part2()
+    {
+        return "D6P2";
+    }
+}
