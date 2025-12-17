dcd502b3e20a56995759d08c3b904b055efd7cbb
Jamie Brown <jamie.brown@ion.fm>
2025-12-17 16:43:27 +0000

    modelling

--- AdventOfCode.2025/Solvers/Day8Solver.cs
+++ AdventOfCode.2025/Solvers/Day8Solver.cs
@@ -16,11 +16,39 @@ public class Day8Solver : IAoCSolver
 
     public string Part1()
     {
-        throw new NotImplementedException();
+        IEnumerable<string> data = _dataProvider.GetData(Day);
+        IEnumerable<Coordinate3D> coordinates = data.Select(Coordinate3D.Parse);
+
+        return "D8P1";
     }
 
     public string Part2()
     {
-        throw new NotImplementedException();
+        return "D8P2";
+    }
+}
+
+public readonly record struct Coordinate3D(long X, long Y, long Z)
+{
+    public static Coordinate3D Parse(string line)
+    {
+        long[] parts = line.Split(',').Select(long.Parse).ToArray();
+        return new Coordinate3D(parts[0], parts[1], parts[2]);
+    }
+
+    public double DistanceTo(Coordinate3D other)
+    {
+        return Math.Sqrt(Math.Pow(X - other.Y, 2) + Math.Pow(Y - other.Z, 2) + Math.Pow(Z - other.Z, 2));
+    }
+}
+
+public class Node
+{
+    public Coordinate3D Position { get; }
+    public List<Node> Connections { get; }
+
+    public double DistanceTo(Node other)
+    {
+        return Position.DistanceTo(other.Position);
     }
 }
