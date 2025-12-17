b1ba04d5964c9983d6d715f856dafcb1c9f42cf9
Jamie Brown <jamie.brown@ion.fm>
2025-12-17 16:30:34 +0000

    scaffold

---
+++ AdventOfCode.2025.Tests/Day8Tests.cs
@@ -0,0 +1,73 @@
+﻿using AdventOfCode2025.DataProviders;
+using AdventOfCode2025.Solvers;
+using FluentAssertions;
+using Moq;
+
+namespace AdventOfCode2025.Tests;
+
+public class Day8Tests
+{
+    private readonly Mock<IDataProvider> _dataProviderMock = new();
+
+    [Theory]
+    [InlineData("40",
+        "162,817,812",
+        "57,618,57",
+        "906,360,560",
+        "592,479,940",
+        "352,342,300",
+        "466,668,158",
+        "542,29,236",
+        "431,825,988",
+        "739,650,466",
+        "52,470,668",
+        "216,146,977",
+        "819,987,18",
+        "117,168,530",
+        "805,96,715",
+        "346,949,466",
+        "970,615,88",
+        "941,993,340",
+        "862,61,35",
+        "984,92,344",
+        "425,690,689")]
+    public void Part1(string expected, params string[] input)
+    {
+        _dataProviderMock.Setup(dp => dp.GetData(8)).Returns(input);
+
+        Day8Solver solver = new(_dataProviderMock.Object);
+
+        solver.Part1().Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("TODO",
+        "162,817,812",
+        "57,618,57",
+        "906,360,560",
+        "592,479,940",
+        "352,342,300",
+        "466,668,158",
+        "542,29,236",
+        "431,825,988",
+        "739,650,466",
+        "52,470,668",
+        "216,146,977",
+        "819,987,18",
+        "117,168,530",
+        "805,96,715",
+        "346,949,466",
+        "970,615,88",
+        "941,993,340",
+        "862,61,35",
+        "984,92,344",
+        "425,690,689")]
+    public void Part2(string expected, params string[] input)
+    {
+        _dataProviderMock.Setup(dp => dp.GetData(8)).Returns(input);
+
+        Day8Solver solver = new(_dataProviderMock.Object);
+
+        solver.Part2().Should().Be(expected);
+    }
+}
---
+++ AdventOfCode.2025/Solvers/Day8Solver.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode2025.DataProviders;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdventOfCode2025.Solvers;
+
+public class Day8Solver : IAoCSolver
+{
+    private readonly IDataProvider _dataProvider;
+
+    public Day8Solver([FromKeyedServices(DataProviderKind.LineDelimited)] IDataProvider dataProvider)
+    {
+        _dataProvider = dataProvider;
+    }
+
+    public int Day => 8;
+
+    public string Part1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Part2()
+    {
+        throw new NotImplementedException();
+    }
+}
